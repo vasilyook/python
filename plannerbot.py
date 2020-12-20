2cd6ff4a3021acc4707c10ff1424e096320858b2
khwabscript <59730462+khwabscript@users.noreply.github.com>
2020-12-20 12:34:48 +0300

    Update plannerbot

--- plannerbot.py
+++ plannerbot.py
@@ -1,56 +1,67 @@
 import telebot
-add_deal = ['добавить','добавь', "+"]
-done_deal = ["выполнила", "Удалить", "удали", "-"]
-view_deal = ["покажи", "показать", "все"]
 
+commands_list = """
+		Оля, вот, что я могу:
+		/help - список команд
+		/add - добавить дело
+		/del - удалить дело
+		/view - что еще осталось
+		"""
+TOKEN = "1492623618:AAGFcPyCSrfGbUjqwrW7KH-ZQjymTdNNkAY"
+bot = telebot.TeleBot(TOKEN)
 
-bot = telebot.TeleBot("токен")
-
-def add(message):
-    text_file = open("test.txt", "a+", encoding = 'utf-8')
-    deal = message.text.lower()
-    deal = '✓' + deal + ';\r'
-    #print(deal)
-    text_file.write(deal)
-    text_file.close()
-
-
-def del_deal(message):
-	text_file = open("test.txt", "r", encoding = 'utf-8')
-	deals = text_file.read()
-    text_file.close()
-    text_file = open("test.txt", "r", encoding = 'utf-8')
-    bot.send_message(message.chat.id, "Какое задание Вы хотите удалить?")
-    #x = int(input('Какое задание Вы хотите удалить? '))
-    lines = text_file.readlines()
-    text_file.close()
-    if lines [x] in lines:
-        del lines [x]
-    text_file = open("test.txt", "w", encoding = 'utf-8')
-    x = ''.join(lines)
-    text_file.write(x)
-    print('Вот, что Вам осталось сделать:')
-    print(x)
-    text_file.close()
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, "Привет, Оля)")
-	#print(message)
+	bot.send_message(message.chat.id, commands_list)
+	
 
+@bot.message_handler(commands=['view'])
+def view_deal(message):
+	text_file = open('test.txt', 'r', encoding = 'UTF-8')
+	lines = text_file.readlines()
+	text_file.close()
+	if len(lines) > 1:
+		bot.send_message(message.chat.id, ''.join(lines))
+	else:
+		bot.send_message(message.chat.id, "Список дел пуст!")	
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text in add_deal:
-        bot.send_message(message.chat.id, 'Что Вам нужно сделать?')
-        # bot.message_handler(content_types=['text'])
-        
+@bot.message_handler(commands=['add'])
+def question_add(message):
+	bot.send_message(message.chat.id, "Что хотите добавить?")
+	# @bot.message_handler(content_types=['text'])
+	@bot.message_handler(regexp='\w*\D+\w*')
+	
+	def add_deal(message):
+		text_file = open('test.txt', 'a+', encoding = 'UTF-8')
+		deal = message.text + '\n'
+		text_file.write(deal)
+		text_file.close()
+		bot.send_message(message.chat.id, "Добавлено:)") 
+	
 
+	
+@bot.message_handler(commands=['del'])
+def question_del(message):
+	bot.send_message(message.chat.id, "Что хотите удалить?")
+	bot.send_message(message.chat.id, "Вот, что у Вас есть:")
+	view_deal(message)
+	# @bot.message_handler(content_types=['text'])
+	@bot.message_handler(regexp='\d+')
 
-    elif message.text.lower() in done_deal:
-        bot.send_message(message.chat.id, 'Что Вы уже сделали?')
-    # elif message.text.lower() in view_deal: 
-    else:
-    	add(message)
+	def del_deal(message):
+		text_file = open('test.txt', 'r', encoding = 'UTF-8')
+		lines = text_file.readlines()
+		text_file.close()
+		try:
+			if lines[int(message.text)] in lines:
+			    del lines[int(message.text)]
+			text_file = open("test.txt", "w", encoding = 'utf-8')
+			x = ''.join(lines)
+			text_file.write(x)
+			text_file.close()
+			bot.send_message(message.chat.id, "Готово!") 
+		except:
+			send_welcome(message)
+
 
 bot.polling()
